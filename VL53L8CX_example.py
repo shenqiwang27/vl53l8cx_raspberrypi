f9527353387fb15dda08b94f77183b4efce46f4b
shenqi <S.wang-18@tudelft.nl>
2024-10-15 14:33:39 +0200

    update

--- VL53L8CX_example.py
+++ VL53L8CX_example.py
@@ -6,8 +6,9 @@ from ctypes import *
 
 tof = VL53L8CX.VL53L8CX()
 
-
-for count in range(1,10):
+count =0
+while True:
+    count +=1
     distance = tof.read_distance()
     if (distance > 0):
         print ("%d mm, %d cm, %d" % (distance, (distance/10), count))
