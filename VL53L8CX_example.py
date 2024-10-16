10662ec9cdc169fdddfba2c40c23edb7922cdfe6
shenqi <S.wang-18@tudelft.nl>
2024-10-16 13:14:56 +0200

    update

--- VL53L8CX_example.py
+++ VL53L8CX_example.py
@@ -2,6 +2,8 @@
 import time
 import VL53L8CX
 from ctypes import *
+import numpy as np
+import cv2
 
 
 tof = VL53L8CX.VL53L8CX()
@@ -10,19 +12,18 @@ count =0
 
 
 def print_8x8_grid(python_array):
-    # Clear the screen
+    
     print("\033[2J")
     
-    # Move cursor to home position
     print("\033[H", end="")
     
     # Print the 8x8 grid
     for i in range(8):
         for j in range(8):
             index = i * 8 + j
-            value = python_array[index] / 1000  # Divide by 100
-            print(f"{value:7.3f}", end="")  # 7 total width, 2 decimal places
-        print()  # New line after each row
+            value = python_array[index] / 1000  
+            print(f"{value:7.3f}", end="")  
+        print()  
     
     # Flush the output
     print(flush=True)
@@ -33,6 +34,13 @@ while True:
     distance = tof.read_distance()
     # print(distance)
     distance_array = [distance[i] for i in range(64)]
-    print_8x8_grid(distance_array)
+    # print_8x8_grid(distance_array)
+    distance_matrix = np.clip(distance_array, 0, 4000)
+    distance_matrix = int(distance_matrix / 15.7)
+    resized_image = cv2.resize(distance_matrix, (400, 400), interpolation=cv2.INTER_NEAREST)
+
+    cv2.imshow('Distance Matrix', resized_image)
+    cv2.waitKey(1)
+
 
 
