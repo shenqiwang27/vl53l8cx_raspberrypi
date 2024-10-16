49a44e5074e51c5ac1eae8f4fddc51829757c725
shenqi <S.wang-18@tudelft.nl>
2024-10-16 13:51:43 +0200

    update

--- VL53L8CX_example.py
+++ VL53L8CX_example.py
@@ -40,7 +40,7 @@ while True:
     distance_matrix = np.clip(distance_array, 0, 4000)
     distance_matrix = distance_matrix / 4000
     distance_matrix = np.uint8(distance_matrix*255)
-    distance_matrix = np.array(distance_array).reshape( 1, 8, 8)
+    distance_matrix = np.array(distance_matrix).reshape(  8, 8)
     print(distance_matrix)
     # resized_image = cv2.resize(distance_matrix, (400, 400), interpolation=cv2.INTER_NEAREST)
     
