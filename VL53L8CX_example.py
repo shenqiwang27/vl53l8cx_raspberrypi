4a3f8a12fca14ae62a909c1d0af44e449672e315
shenqi <S.wang-18@tudelft.nl>
2024-10-31 09:50:15 +0100

    update

--- VL53L8CX_example.py
+++ VL53L8CX_example.py
@@ -36,13 +36,17 @@ while True:
     target_status_array = [target_status[i] for i in range(64)]
     print_8x8_grid(distance_array,target_status_array)
     
-    distance_matrix = np.clip(np.where(np.array(distance_array) < 0, 4000, distance_array), 0, 4000)
-    distance_matrix = distance_matrix / 4000
-    distance_matrix = np.uint8(distance_matrix*255)
-    distance_matrix = np.array(distance_matrix).reshape(  8, 8)
-    # print(distance_matrix)
-    # resized_image = cv2.resize(distance_matrix, (400, 400), interpolation=cv2.INTER_NEAREST)
+    distance_array = np.array(distance_array)
+    target_status_array = np.array(target_status_array)
+    distance_array = np.where(target_status_array != 5, 4000, distance_array)
     
+    distance_matrix = np.clip(np.where(distance_array < 0, 4000, distance_array), 0, 4000)
+    
+    distance_matrix = distance_matrix / 4000
+    distance_matrix = np.uint8(distance_matrix * 255)
+    distance_matrix = np.array(distance_matrix).reshape(8, 8)
+    
+    # 显示图像
     cv2.imshow('Distance Matrix', distance_matrix)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
